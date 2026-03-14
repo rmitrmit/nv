867dc3d9b91b561521eb97d4c7cbeabc4ef39bc7
rmitrmit <142961281+rmitrmit@users.noreply.github.com>
2026-03-14 10:07:48 +0700

    message

--- src/app/change-lyrics/page.tsx
+++ src/app/change-lyrics/page.tsx
@@ -390,7 +390,7 @@ function ChangeLyricsPageContent() {
 
                         {/* Lyrics editor */}
                         <div className="flex flex-col gap-3">
-                            <label className="text-sm font-bold tracking-[0.18em] uppercase text-[#8b1a1a]/70">Edit your desired lyrics below</label>
+                            <label className="text-sm font-bold tracking-[0.18em] uppercase text-[#8b1a1a]/70">Edit your desired lyrics in the text editor below</label>
                             <LyricsEditor
                                 value={formValues.lyrics}
                                 originalValue={originalLyricsText}
